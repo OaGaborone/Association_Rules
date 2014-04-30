0820326c6e462b4ea3381910162eedd32147d3e2
yichiliu <yl3057@columbia.edu>
2014-04-30 03:37:39 -0400

    checkpoint first round dict parsing

--- apriori.py
+++ apriori.py
@@ -1,16 +1,48 @@
+# from __future__ import print_function
 from pprint import pprint
 
-def processTransaction(transactions):
+def processTransactions(transactions,minsup,minconf):
 	"""Processes transactions, returns printable data structures
+	
 	TODO add more documentation
-	"""
-	return _processTransaction(False,transactions)	
 
-def _processTransaction(DEBUG,transactions):
+	"""
+	return _processTransactions(False,transactions,minsup,minconf)	
+
+def _processTransactions(DEBUG,transactions,minsup,minconf):
+
+	transSize = len(transactions)
+	dicts=[]
+	d={}
+	for tran in transactions:
+		for itemSet in tran.values():
+			for item in itemSet:
+				# print item
+				if frozenset([item]) in d:
+					d[frozenset([item])]+=1
+				else:
+					d[frozenset([item])]=1
+
+	dicts.append(d)
+	
+	# log = open("./log.txt", "w")
+	# print(d, file = log)
+	# log.close()
+
+	pprint(dicts[0])
+
+
+
 
 	if DEBUG:
 		pprint(transactions)
 
 
+
+
+
+
+
+
 if __name__ == "__main__":
-	_processTransaction(True,None)
+	_processTransactions(True,None,0.3,0.3)
--- rules.py
+++ rules.py
@@ -1,13 +1,19 @@
+from __future__ import print_function
 from datasetparser import getTransactions
-from apriori import processTransaction
+from apriori import processTransactions
 from pprint import pprint
+
 
 minsup=0.1
 minconf=0.1
 
 if __name__ == "__main__":
 
-	trans=getTransactions(True,100)
+	# trans=getTransactions(True,500)
+	trans=getTransactions(False,0)
 
-	processTransaction(trans)
 
+	processTransactions(trans,minsup,minconf)
+
+	# log = open("./log.txt", "w")
+	# print(processTransactions(trans,minsup,minconf), file = log)
